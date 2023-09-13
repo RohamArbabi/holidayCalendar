be24866c67000969d6771869e93c1cbeb4ecb1f8
RohamArbabi <115377208+RohamArbabi@users.noreply.github.com>
2023-09-13 09:37:12 -0700

    Function for local date implemented

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,14 +1,15 @@
-//Holiday calendar
+package main;//Holiday calendar
 //test
-
+import java.time.LocalDate;
 
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Use a function that stores the Months and days and current date
+        System.out.println(LocalDate.now());
     }
 
-    //Make a function that stores the Months and days and current date
+
     //Make an array or some kind data storage that holds all the national holidays
     //Make a GUI that uses the Months and days function and shows the current date
     //Apply the national holidays
