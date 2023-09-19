39b636cb0b2aee8b0235ee89586d0716833bb1b5
Ishaan Kendale <143761057+Eyeshaank@users.noreply.github.com>
2023-09-19 10:17:38 -0700

    Made j-frame

--- src/gui/GUI.java
+++ src/gui/GUI.java
@@ -1,4 +1,23 @@
 package gui;
-
+import java.awt.BorderLayout;
+import java.awt.Button;
+import java.awt.Component;
+import java.awt.Frame;
+import java.awt.TextArea;
+import main.Main;
 public class GUI {
+
+    public static void main(String[] args) {
+        Frame frame = new Frame("Holiday Calendar");
+        Component textArea = new TextArea("E");
+        Component button = new Button("Click this");
+        frame.add(textArea, BorderLayout.NORTH);
+        frame.add(button, BorderLayout.SOUTH);
+        int width = 300;
+        int height = 300;
+        frame.setSize(width, height);
+
+        frame.setVisible(true);
+
+    }
 }
