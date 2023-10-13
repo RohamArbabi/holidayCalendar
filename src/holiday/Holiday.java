baf2167a5cb70fcd7b37f40b552bcde980b7f244
Imirami314 <98506195+Imirami314@users.noreply.github.com>
2023-10-13 09:26:14 -0700

    oh yeah hupen dingle
    gua

--- src/holiday/Holiday.java
+++ src/holiday/Holiday.java
@@ -5,17 +5,11 @@ import java.util.Arrays;
 
 public class Holiday {
     public int m;
-    public int[] d;
+    public int d;
     public String desc;
 
 
     public Holiday(int m, int d, String desc) {
-        this.m = m;
-        this.d = new int[]{d};
-        this.desc = desc;
-    }
-
-    public Holiday(int m, int[] d, String desc) {
         this.m = m;
         this.d = d;
         this.desc = desc;
@@ -23,7 +17,7 @@ public class Holiday {
 
     @Override
     public String toString() {
-        return "Month: " + m + ", Day: " + Arrays.toString(d) + "\nDescription:\n" + desc;
+        return "Month: " + m + ", Day: " + d + "\nDescription:\n" + desc;
     }
 }
 
