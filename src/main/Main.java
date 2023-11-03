cf4b30fdb2e770c210ad575dfa7e8d31f4b2c51d
RohamArbabi <115377208+RohamArbabi@users.noreply.github.com>
2023-11-03 09:56:59 -0700

    guld

--- src/main/Main.java
+++ src/main/Main.java
@@ -20,18 +20,18 @@ public class Main {
 //        //Holiday memorialDay = new Holiday(5, new int[]{25,26,27,28,29,30,31}, "Honors U.S. military personnel who have fought and died while serving in the United States Armed Forces.");
 //        Holiday juneteenthIndependanceDay = new Holiday(6,19,"Commemorates the emancipation of enslaved African Americans on the anniversary of the 1865 date when emancipation was announced in Galveston, Texas.");
 //        Holiday independenceDay = new Holiday(7,4,"Celebrates the 1776 adoption of the Declaration of Independence from British rule.");
-        Holiday laborDay = new Holiday(9, Year.dateFromWeekdayNumber(9, curYear, 2, 1).getDay(), "Honors and recognizes the American labor movement.");
+        //Holiday laborDay = new Holiday(9, Year.dateFromWeekdayNumber(9, curYear, 2, 1).getDay(), "Honors and recognizes the American labor movement.");
 //        Holiday indigenousPeoplesDay = new Holiday(10, Year.dateFromWeekdayNumber(10, curYear, 2, 2).getDay(), "Indigenous Peoples' Day is a holiday that celebrates and honors indigenous American peoples and commemorates their histories and cultures.");
 //        Holiday veteransDay = new Holiday(11, 11, "Honors all veterans of the United States armed forces.");
 
-        //Holiday thanksgivingDay = new Holiday(11, Year.dateFromWeekdayNumber(11, curYear, 5, 4).getDay(), "Traditionally celebrates the giving of thanks for the autumn harvest, and commonly includes the sharing of a turkey dinner.");
+        Holiday thanksgivingDay = new Holiday(11, Year.dateFromWeekdayNumber(11, curYear, 5, 4).getDay(), "Traditionally celebrates the giving of thanks for the autumn harvest, and commonly includes the sharing of a turkey dinner.");
         //Holiday christmasDay = new Holiday(12, 25, "Christmas is an annual festival commemorating the birth of Jesus Christ.");
 
 
         //Use a function that stores the months and days and current date
 
         //new GUI();
-        System.out.println(laborDay);
+        System.out.println(thanksgivingDay);
 
     }
 
